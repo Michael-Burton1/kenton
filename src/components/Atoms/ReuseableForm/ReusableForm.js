62611aa6019a03de2117af8efeae9437ab01ecbc
Michael-Burton1 <76928439+Michael-Burton1@users.noreply.github.com>
2022-09-26 13:27:03 -0700

    add divs for reusable form for easy styling

--- src/components/Atoms/ReuseableForm/ReusableForm.js
+++ src/components/Atoms/ReuseableForm/ReusableForm.js
@@ -1,17 +1,40 @@
 import React from "react";
 import PropTypes from "prop-types";
+import '../ReuseableForm/reusableForm.scss';
 
 function ReusableForm(props) {
   return (
     <React.Fragment>
-      <form onSubmit={props.formSubmissionHandler}>
-        <input type="text" name="date" placeholder="Location" />
-        <input type="text" name="name" placeholder='Headliner Name' />
-        <input type="text" name="support" placeholder="support name" />
-        <input type="text" name="photo" placeholder="photo" />
-        <input type="text" name="time" placeholder="time" />
-        <input type="text" name="price" placeholder="price" />
-        <button type='submit'>{props.buttonText}</button>
+      <form className="reusableForm" onSubmit={props.formSubmissionHandler}>
+        <div className="reusableFormInputs">
+          <div className="formInputDate">
+            <label name='date'>Date: </label>
+            <input type="text" name="date" placeholder="01/30/2001" />
+          </div>
+          <div className="formInputHeadliner">
+            <label name='name'>Headliner:  </label>
+            <input type="text" name="name" placeholder='Headliner Name' />
+          </div>
+          <div className="formInputSupport">
+            <label name='support'>Support: </label>
+            <input type="text" name="support" placeholder="support name" />
+          </div>
+          <div className="formInputPhoto">
+            <label name='name'>Photo:  </label>
+            <input type="text" name="photo" placeholder="photo" />
+          </div>
+          <div className="formInputTime">
+            <label name='date'>Show time: </label>
+            <input type="text" name="time" placeholder="7:00" />
+          </div>
+          <div className="formInputPrice">
+            <label name='name'>Price:  </label>
+            <input type="text" name="price" placeholder='10 adv / 12 dos' />
+          </div>
+        </div>
+        <div className="reusableFromButton">
+          <button type='submit'>{props.buttonText}</button>
+        </div>
       </form>
     </React.Fragment>
   );
