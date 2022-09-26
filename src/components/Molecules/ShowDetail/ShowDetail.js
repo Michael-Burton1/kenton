c83765aae4b8c99454c47198443d8c1ae329115e
Michael-Burton1 <76928439+Michael-Burton1@users.noreply.github.com>
2022-09-26 09:20:38 -0700

    add html for show detail

--- src/components/Molecules/ShowDetail/ShowDetail.js
+++ src/components/Molecules/ShowDetail/ShowDetail.js
@@ -1,11 +1,42 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import '../ShowDetail/showDetail.scss'
 
 function ShowDetail(props) {
+  const { show }= props;
+
   return (
     <div>
-      <h1>tix detail</h1>
+      <h1>Show details</h1>
+      <div className='showDetail'>
+        <div className='showDetailDate'>
+          <p>date: </p><p>{show.date}</p>
+        </div>
+        <div className='showDetailName'>
+          <p>headliner: </p><p>{show.name}</p>
+        </div>
+        <div className='showDetailSupport'>
+          <p>support: </p><p>{show.support}</p>
+        </div>
+        <div className='showDetailPhoto'>
+          <p>photo:  </p><p>{show.photo}</p>
+        </div>
+        <div className='showDetailTime'>
+          <p>time: </p><p>{show.time}</p>
+        </div>
+        <div className='showDetailPrice'>
+          <p>price: </p><p>{show.price}</p>
+        </div>
+        <div className='showDetailId'>
+          <p>showID: </p><p>{show.id}</p>
+        </div>
+      </div>
     </div>
   );
+}
+
+ShowDetail.propTypes = {
+  show: PropTypes.object
 }
 
 export default ShowDetail;
